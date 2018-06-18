2e6af3da9374092e1397d9b7c24923f63b1e6ded
OxiBo <xenos2000@ukr.net>
2018-06-18 13:27:09 -0400

    add last changed to js file

--- js/todolist.js
+++ js/todolist.js
@@ -1,14 +1,30 @@
-const $item = $("li");
-const $delete = $("span");
+const $list = $("ul");
+const $item = "li";
+const $delete = "span";
+const $input = $("input[type='text']");
+const $plus = $(".fa-plus");
 
-$item.on("click", function() {
+$list.on("click", $item, function() {
     $(this).toggleClass("completed");
 })
 
-$delete.on("click", function(event) {
+$list.on("click", $delete, function(event) {
     event.stopPropagation();
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
 })
+
+$input.on("keypress", function(event) {
+if(event.which === 13) {
+    let todoText = $input.val();
+    $(this).val("");
+    $("ul").append(`<li><span><i class="fas fa-trash-alt"></i></span>${todoText}`);
+}
+})
+
+$plus.on("click", function() {
+    $input.fadeToggle();
+})
+
 
